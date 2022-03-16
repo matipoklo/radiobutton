8b177c9308c8976ffad5e50606f47eec97167559
matipoklo <poklojr@gmail.com>
2022-03-16 08:12:15 +0100

    ZROBIONY PROGRAM

--- app/src/main/java/com/example/radiobutton/MainActivity.kt
+++ app/src/main/java/com/example/radiobutton/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.example.radiobutton
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        var radioGroup: RadioGroup? = null
+        lateinit var radioButton: RadioButton
+        private lateinit var button: Button
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            title = "KotlinApp"
+            radioGroup = findViewById(R.id.radioGroup)
+            button = findViewById(R.id.btnDisplay)
+            button.setOnClickListener {
+                val intSelectButton: Int = radioGroup!!.checkedRadioButtonId
+                radioButton = findViewById(intSelectButton)
+                Toast.makeText(baseContext, radioButton.text, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
-}
